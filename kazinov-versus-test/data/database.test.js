de03486d6df2a26eccd51c95fcb2d9c85e11c486
kazinov <dm.kazinov@gmail.com>
2015-08-18 23:15:43 +0300

    test: add test to database

--- kazinov-versus-test/data/database.test.js
+++ kazinov-versus-test/data/database.test.js
@@ -30,4 +30,32 @@ describe("module: database", function () {
             });
         });
     });
+
+    describe("method: findByNameUrl()", function () {
+        it("should return proper data when one name url specified", function (done) {
+            database.findByNameUrl(["qualcomm-snapdragon-800"], function (err, data) {
+                var filtered = data.filter(function(o) {
+                    return o.name === "Qualcomm Snapdragon 800 MSM8974AA v2";
+                })
+                assert.ok(filtered.length === 1);
+
+                done();
+            });
+        });
+
+        it("should return proper data when multiple name urls specified", function (done) {
+            database.findByNameUrl(["android-4-4-kitkat", 'nexus-5'], function (err, data) {
+                var filtered = data.filter(function(o) {
+                    return o.name === "Android 4.4 KitKat";
+                })
+                assert.ok(filtered.length === 1);
+
+                filtered = data.filter(function(o) {
+                    return o.name === 'Nexus 5';
+                })
+                assert.ok(filtered.length === 1);
+                done();
+            });
+        });
+    });
 });
