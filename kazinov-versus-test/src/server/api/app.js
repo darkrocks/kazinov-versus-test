283afbde42b5624e0d21f7b37b5c25cd201f3a11
kazinov <dm.kazinov@gmail.com>
2015-08-19 08:23:04 +0300

    server improvements

--- kazinov-versus-test/src/server/api/app.js
+++ kazinov-versus-test/src/server/api/app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var routes = require('./routes');
 
 var PORT = 3000;
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 routes.configure(app);
+
 
 var server = app.listen(PORT, function () {
     var host = server.address().address;
