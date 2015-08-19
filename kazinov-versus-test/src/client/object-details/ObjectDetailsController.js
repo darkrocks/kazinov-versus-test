61d1afff8a479bb198299f82f6e385a9a50dfe5c
kazinov <dm.kazinov@gmail.com>
2015-08-19 12:41:37 +0300

    asdfasd

--- kazinov-versus-test/src/client/object-details/ObjectDetailsController.js
+++ kazinov-versus-test/src/client/object-details/ObjectDetailsController.js
@@ -5,47 +5,13 @@
         dataService.findByNameUrl(nameUrl)
             .then(function (object) {
                 $scope.object = object;
-            });
-
-        /*
-         this.$scope.object = {
-         name: 'Android 4.4 KitKat',
-         properties: {
-         '4k': true,
-         'widgets': true,
-         'apps': 1000000,
-         'opengl_es_3_0': true,
-         'customize_notifications': true,
-         'offline_voice_recognition': true
-         },
-         embeddedObjects: [
-         {
-         name: '2222',
-         properties: {
-         '4k': true,
-         'widgets': true,
-         'apps': 1000000,
-         'opengl_es_3_0': true,
-         'customize_notifications': true,
-         'offline_voice_recognition': true
-         },
-         embeddedObjects: [
-         {
-         name: '3333',
-         properties: {
-         '4k': true,
-         'widgets': true,
-         'apps': 1000000,
-         'opengl_es_3_0': true,
-         'customize_notifications': true,
-         'offline_voice_recognition': true
-         },
-         embeddedObjects: []
-         }
-         ]
-         }
-         ]
-         };*/
+            },
+            function (error) {
+                if (error.status === 404) {
+                    $state.go('404');
+                }
+            }
+        );
     }
 
     ObjectDetailsController.$inject = [
--- kazinov-versus-test/src/client/routes.js
+++ kazinov-versus-test/src/client/routes.js
@@ -4,10 +4,18 @@
         $locationProvider.html5Mode(true);
         $urlRouterProvider.otherwise('/');
         $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'src/client/templates/home.html'
+            })
             .state('object-details', {
                 url: '/{nameUrl}',
                 templateUrl: 'src/client/object-details/object-details.html',
                 controller: 'object-details.controller'
+            })
+            .state('404', {
+                url: '/error/404',
+                templateUrl: 'src/client/templates/404.html'
             });
     }
 
