7c42a17fa1197fb3bc0a28286f9ce8b72416276d
kazinov <dm.kazinov@gmail.com>
2015-08-19 10:02:14 +0300

    client app start

---
+++ kazinov-versus-test/src/client/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by admin on 19.08.2015.
+ */
--- kazinov-versus-test/src/server/app.js
+++ kazinov-versus-test/src/server/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 var routes = require('./routes');
 
 var PORT = 3000;
 
 var app = express();
+app.use(express.static(path.join(__dirname, '../..')));
+console.log(path.join(__dirname, '../..'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes.configure(app);
@@ -17,3 +20,6 @@ var server = app.listen(PORT, function () {
 });
 
 module.exports = app;
+
+
+//C:\123123123\kazinov-versus-test\node_modules
